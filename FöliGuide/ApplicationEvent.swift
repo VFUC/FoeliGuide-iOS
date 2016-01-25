e9a7146c1fd0814b7a742c6207b72cd1d8ae468d
Jonas Wippermann <jonas@vfuc.co>
2016-01-25 11:36:21 +0200

    Adding ApplicationEvent enum + Handling in AppDelegate

---
+++ FöliGuide/ApplicationEvent.swift
@@ -0,0 +1,15 @@
+//
+//  ApplicationEvents.swift
+//  FöliGuide
+//
+//  Created by Jonas on 25/01/16.
+//  Copyright © 2016 Capstone Innovation Project - Route Guidance. All rights reserved.
+//
+
+import Foundation
+
+enum ApplicationEvent {
+	case LocationServicesDisabled
+	case LocationAuthorizationNotDetermined
+	case LocationAuthorizationDenied
+}
