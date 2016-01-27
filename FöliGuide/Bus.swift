b9e6f0934513d6c58f857f6fc8f8b424b665ea7b
Jonas Wippermann <jonas@vfuc.co>
2016-01-27 10:49:49 +0200

    Adding Bus/BusStop structs

---
+++ FöliGuide/Bus.swift
@@ -0,0 +1,17 @@
+//
+//  Bus.swift
+//  FöliGuide
+//
+//  Created by Jonas on 27/01/16.
+//  Copyright © 2016 Capstone Innovation Project - Route Guidance. All rights reserved.
+//
+
+import UIKit
+import CoreLocation
+
+struct Bus {
+	var location: CLLocation
+	var name: String
+	var nextStop: BusStop
+	//TODO: Estimated arrival
+}
---
+++ FöliGuide/BusStop.swift
@@ -0,0 +1,16 @@
+//
+//  BusStop.swift
+//  FöliGuide
+//
+//  Created by Jonas on 27/01/16.
+//  Copyright © 2016 Capstone Innovation Project - Route Guidance. All rights reserved.
+//
+
+import Foundation
+import CoreLocation
+
+struct BusStop {
+	var name: String
+	var number: String
+	var location: CLLocation?
+}
