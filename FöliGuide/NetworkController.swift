c40731d14c5422fa4ccab5fefd1cda06040cae2f
Jonas Wippermann <jonas@vfuc.co>
2016-02-01 10:15:17 +0200

    Adding ability to cancel any running request in network controller

--- FöliGuide/NetworkController.swift
+++ FöliGuide/NetworkController.swift
@@ -12,15 +12,17 @@ import SwiftyJSON
 
 class NetworkController: NSObject {
 	
+	static var requestActive = false
+	static var latestRequest : Request?
 	
-	 class func getCurrentRealtimeBusData(completionHandler: JSON? -> () ){
+	class func getCurrentRealtimeBusData(completionHandler: JSON? -> () ){
 		
 		getDataFromAPI { apiData -> () in
 			guard let apidata = apiData else { //check if data returns nil
 				completionHandler(nil)
 				return
 			}
-	
+			
 			let json = JSON(data: apidata)
 			completionHandler(json)
 		}
@@ -28,12 +30,24 @@ class NetworkController: NSObject {
 	
 	
 	private class func getDataFromAPI(completionHandler: NSData? -> () ) {
-		Alamofire.request(.GET, Constants.API.RealTimeBusURL)
+		requestActive = true
+		
+		latestRequest = Alamofire.request(.GET, Constants.API.RealTimeBusURL)
 			.responseData { response in
+				
+				requestActive = false
+				
 				print(response.request)
 				print(response.response)
 				print(response.result)
 				completionHandler(response.data)
+		}
+	}
+	
+	
+	class func cancelActiveRequest() {
+		if requestActive {
+			latestRequest?.cancel()
 		}
 	}
 }
