4833094f2a7c244b65f2501840c4ae8e0ee5d126
Jonas Wippermann <jonas@vfuc.co>
2016-04-07 12:32:33 +0300

    Implementing next stop sub view controller, no alarm yet

--- FöliGuide/NextStopSubViewController.swift
+++ FöliGuide/NextStopSubViewController.swift
@@ -9,27 +9,76 @@
 import UIKit
 
 class NextStopSubViewController: UIViewController {
+	
+	let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
 
+	
+	@IBOutlet weak var nextStationNameLabel: UILabel!
+	@IBOutlet weak var afterThatStationNameLabel: UILabel!
+	@IBOutlet weak var selectedBusStationNameLabel: UILabel!
+	
+	@IBOutlet weak var mainStackView: UIStackView!
+	@IBOutlet weak var selectedBusStationStackView: UIStackView!
+	
+	
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+		
+		nextStationNameLabel.text = ""
+		afterThatStationNameLabel.text = ""
+		appDelegate.busDataUpdateDelegates.append(self)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
+
+// Data has been updated, check if notification is necessary
+extension NextStopSubViewController : BusUpdateDelegate {
+	func didUpdateBusData(){
+		
+		guard let bus = appDelegate.busController.currentUserBus else {
+			return
+		}
+		
+		guard let detailVC = self.parentViewController as? BusDetailViewController else {
+			print("[NextStopVC] My parent VC is not a busDetailVC (which I expected)")
+			return
+		}
+		
+		
+		guard detailVC.busNumberLabel.text != bus.name || nextStationNameLabel.text != bus.nextStop.name else {
+			//Data did not change
+			return
+		}
+		
+//		detailVC.busNumberLabel.text = bus.name
+//		finalStationName.text = bus.finalStop
+		
+		nextStationNameLabel.text = bus.nextStop.name
+		afterThatStationNameLabel.text = bus.afterThatStop?.name ?? "--"
+		
+		afterThatStationNameLabel.hidden = (nextStationNameLabel.text == afterThatStationNameLabel.text) //Hide if both labels are equal
+		
+		
+		/*
+		if nextStationNameLabel.text == destinationStop {
+			NotificationController.showNextBusStationNotification(stopName: nextStationNameLabel.text!, viewController: self)
+			destinationStop = nil
+		}
+		
+		if afterThatStationNameLabel.text == destinationStop {
+			NotificationController.showAfterThatBusStationNotification(stopName: afterThatStationNameLabel.text!, viewController: self)
+		}
+		
+		
+		if volumeEnabled {
+			if let nextStation = nextStationNameLabel.text {
+				SpeechController.announceNextBusStop(nextStation)
+			}
+			
+			if let afterThatStation = afterThatStationNameLabel.text {
+				SpeechController.announceFollowingBusStop(afterThatStation)
+			}
+		}
+		*/
+	}
+}
+
