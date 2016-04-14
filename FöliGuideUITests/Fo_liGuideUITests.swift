527cef27c231dbc1c84755ccf01b67b04730ce1f
Jonas Wippermann <jonas@vfuc.co>
2016-04-14 09:54:42 +0300

    Adding snapshot components

--- FöliGuideUITests/Fo_liGuideUITests.swift
+++ FöliGuideUITests/Fo_liGuideUITests.swift
@@ -9,28 +9,44 @@
 import XCTest
 
 class Fo_liGuideUITests: XCTestCase {
-        
+	
+	let app = XCUIApplication()
+	
     override func setUp() {
         super.setUp()
-        
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-        
-        // In UI tests it is usually best to stop immediately when a failure occurs.
-        continueAfterFailure = false
-        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
-        XCUIApplication().launch()
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+		
+		setupSnapshot(app)
+		app.launch()
     }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
+	
     
     func testExample() {
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+		snapshot("0Launch")
+		app.navigationBars["Fo_liGuide.MainView"].buttons["gear icon barbuttonsize"].tap()
+		
+		snapshot("1Settings")
+		let tablesQuery2 = app.tables
+		tablesQuery2.buttons["Done"].tap()
+		app.buttons["bus"].tap()
+		
+		snapshot("2BusSelectionScreen")
+		
+		app.tables.cells.elementBoundByIndex(0).tap()
+		
+		snapshot("3BusDetailScreen")
+		
+		let foLiguideBusdetailviewNavigationBar = app.navigationBars["Fo_liGuide.BusDetailView"]
+		foLiguideBusdetailviewNavigationBar.buttons["bell"].tap()
+
+		snapshot("4DestinationSelectionScreen")
+		
+		
+		//		tablesQuery.staticTexts["Urheilutie"].tap()
+//		foLiguideBusdetailviewNavigationBar.buttons["bell filled"].tap()
+//		app.alerts["Remove alarm?"].collectionViews.buttons["Remove"].tap()
+//		app.buttons["ios volume low"].tap()
+//		foLiguideBusdetailviewNavigationBar.childrenMatchingType(.Button).matchingIdentifier("Back").elementBoundByIndex(0).tap()
     }
-    
+	
 }
+
